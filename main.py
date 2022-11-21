f0a4fa4353b971d841682f027f10ab02f5cdac63
blaze <blazejos1230@gmail.com>
2022-11-21 21:28:00 +0100

    Update paths

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     db = get_database()
     collection_name = db["twitch_games"]
 
-    df = pd.read_csv(r'../Project1/data/Twitch_game_data.csv', encoding='cp1252')
+    df = pd.read_csv(r'./data/Twitch_game_data.csv', encoding='cp1252')
     print(df)
 
     collection_name.insert_many(df.to_dict('records'))
