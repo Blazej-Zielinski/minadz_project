805edd707d0b0dd6afc08294c45ad1398e64b71b
blaze <blazejos1230@gmail.com>
2022-11-24 20:25:57 +0100

    Secret

--- mongoDBConnector.py
+++ mongoDBConnector.py
@@ -1,11 +1,8 @@
 from pymongo import MongoClient
+from secret import CONNECTION_STRING
 
 
 def get_database():
-    # Provide the mongodb atlas url to connect python to mongodb using pymongo
-    # CONNECTION_STRING = "mongodb+srv://user:user13@minadzd.fwbuzoz.mongodb.net/test"
-    CONNECTION_STRING = "mongodb+srv://User1:Banan123!@cluster0.bkbovyp.mongodb.net/test"
-
     # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
     client = MongoClient(CONNECTION_STRING)
 
